1f0a617a009e17fee9e369c7f052cefb099cbd38
Uzedzhi <hello@DESKTOP-46MP7BL.localdomain>
2026-05-13 19:16:21 +0300

    added better testing script

--- RunTests.sh
+++ RunTests.sh
@@ -1,49 +1,98 @@
 #!/bin/bash
-SANITIZER_FLAGS="-fsanitize=address,alignment,bool,bounds,enum,float-cast-overflow,float-divide-by-zero,integer-divide-by-zero,leak,nonnull-attribute,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,undefined,unreachable,vla-bound,vptr"
-PROFILER_FLAGS="-O2 -DNDEBUG -msse4.2 -mavx2"
-SPEED_FLAGS="-O3 -DNDEBUG -msse4.2 -mavx2"
-LIBS_L_PATH="-L/mnt/c/Users/Azerty/my_project/my_libs -Wl,-rpath=/mnt/c/Users/Azerty/my_project/my_libs/"
-INCLUDES_PATH="-I/mnt/c/Users/Azerty/my_project/my_libs/ -Iincludes/"
+# AsciiSum_Hash_E Rollleft_Hash_E Rollright_Hash_E SDBM_Hash_E CRC32_Hash_E FNV1A_Hash_E FirstAsciiChar_Hash_E Strlen_Hash_E AlwaysZero_Hash_E
 
-ALL_CPP_FILES=" src/list.cpp                src/hash.cpp            src/list_dump.cpp       \
-                src/hashfuncs.cpp           src/hashdump.cpp        src/HashAssembly.cpp  \
-                ../my_libs/error_manage.cpp"
+PROGRAM_RUN_CMD="./bin/hash texts/usa.txt"
+INCLUDES_PATH="-Iincludes/ -I../my_libs/ -Imnt/c/users/azerty/my_project/my_libs/"
+ALL_HASH_FUNCS="AsciiSum_Hash_E Rollleft_Hash_E Rollright_Hash_E SDBM_Hash_E CRC32_Hash_E FNV1A_Hash_E"
+ALL_CPP_FILES="src/hashdump.cpp src/hashfuncs.cpp src/list.cpp src/list_dump.cpp src/HashAssembly.cpp src/hash.cpp"
+TEST_DIR_NAME="PROFILER_TEST"
+SPEED_FLAGS="-O3 -mavx2 -msse4.2 -g"
+DUMP_FLAGS="-O3 -mavx2 -msse4.2 -DDUMP_ENABLE -g"
+FLAGS="-O3 -mavx2 -msse4.2"
 
-ALL_HASH_FUNCS="AsciiSum_Hash_E Rollleft_Hash_E Rollright_Hash_E SDBM_Hash_E CRC32_Hash_E FNV1A_Hash_E AlwaysZero_Hash_E FirstAsciiChar_Hash_E Strlen_Hash_E"
-MODDED_HASH_FUNCS="StrlenIntrinsic_Hash_E CRC32Intrinsic_Hash_E"
-PROGRAM_RUN_COMMAND="./bin/hash texts/usa.txt"
+run_profiler() {
+    mkdir -p "callgrind/${TEST_DIR_NAME}"
+    valgrind --tool=callgrind\
+             --callgrind-out-file=callgrind/${TEST_DIR_NAME}/callgrind.out.$1 \
+             $PROGRAM_RUN_CMD
+}
 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++Repeat with 3 types of program++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-for TYPE in NOP STRCMPOP ALLOP
-do
-    if [ "$TYPE" = "ALLOP" ]; then
-    CUR_HASH_FUNCS=$MODDED_HASH_FUNCS
-    else
-    CUR_HASH_FUNCS=$ALL_HASH_FUNCS
+run_speedtest() {
+    mkdir -p "listdump/${TEST_DIR_NAME}"
+    mkdir -p "hyperfine/${TEST_DIR_NAME}"
+    hyperfine "$PROGRAM_RUN_CMD" --warmup 5 --runs 20 --export-csv hyperfine/${TEST_DIR_NAME}/$1.txt
+}
+
+run_dump() {
+    mkdir -p "listdump/${TEST_DIR_NAME}"
+    $PROGRAM_RUN_CMD
+}
+
+compile_nasm() {
+    nasm -f elf64 src/FuncImpl/$1.asm -o $1.o
+}
+
+compile_file() {
+    g++ $FLAGS src/FuncImpl/$1.cpp -c $1.o
+}
+
+rm_trash() {
+    rm *.o
+    rm bin/hash
+}
+
+compile_program() {
+    rm_trash
+
+    compile_nasm "MyCRC32_ASM"
+    compile_nasm "MyFNV1A_ASM"
+    compile_nasm "MySDBM_ASM"
+
+    if [ $1 == "ASM" ]; then
+        compile_nasm "MyStrlen_ASM"
+        compile_nasm "MyStrcmp_ASM"
+    elif [ $1 == "ASMINLINE" ]; then
+        compile_file "MyStrcmp_ASMINLINE"
+        compile_file "MyStrlen_ASMINLINE"
     fi
-    # ==================================================================
-    # ==============1st uses valgrind to profile the program============
-    # ================2nd uses hyperfine to test its speed==============
-    # =======3rd runs through the program but dumps every hash func=====
-    # ==================================================================
-    for FuncIdx in $CUR_HASH_FUNCS
+
+    g++ $LIBS_L_PATH $INCLUDES_PATH                   \
+        $2 -DUSE_$1                                   \
+        -c $ALL_CPP_FILES
+    
+    g++ $LIBS_L_PATH $INCLUDES_PATH                   \
+        $2 -DUSE_$1                                   \
+        *.o -o bin/hash
+     rm *.o
+}
+
+
+
+if [ $2 == "SPEED" ]; then
+    compile_program $1 $SPEED_FLAGS
+    for FuncIdx in $ALL_HASH_FUNCS
     do
-        rm bin/hash
-        nasm -f elf64 src/MyStrLen.asm -o MyStrLen.o
-        
-        g++ $LIBS_L_PATH $INCLUDES_PATH                                                         \
-            $PROFILER_FLAGS -DINIT_FUNC_IDX=$FuncIdx -DUSE_$1                                   \
-            -c $ALL_CPP_FILES
-        
-        g++ $LIBS_L_PATH $INCLUDES_PATH                                                         \
-            $PROFILER_FLAGS -DINIT_FUNC_IDX=$FuncIdx -DUSE_$1                                   \
-            *.o -o bin/hash
-        
-        valgrind --tool=callgrind --callgrind-out-file=callgrind/${TYPE}/callgrind.out.${TYPE}_${FuncIdx} $PROGRAM_RUN_COMMAND
-        hyperfine "$PROGRAM_RUN_COMMAND" --warmup 5 --runs 20 --export-markdown results_${TYPE}_${FuncIdx}.md
-        $PROGRAM_RUN_COMMAND -DDUMP_HASH
-        rm *.o
+        echo -e                                                 \
+        "\e[0;35m                                               \
+        \n=================================================     \
+                \n\tnow testing func $FuncIdx                   \
+        \n=================================================     \
+        \n\e[0m"
+
+        run_profiler    "$FuncIdx"
+        run_speedtest   "$FuncIdx"
     done
-done
+elif [ $2 == "DUMP" ]; then
+    compile_program $1 $DUMP_FLAGS
+    for FuncIdx in $ALL_HASH_FUNCS
+    do
+        echo -e                                                 \
+        "\e[0;35m                                               \
+        \n=================================================     \
+                \n\tnow testing func $FuncIdx                   \
+        \n=================================================     \
+        \n\e[0m"
+
+        run_dump "$FuncIdx"
+    done
+fi
